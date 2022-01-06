f56ae4b6672c2194dd8e763ea8e5a7896352cd73
script-beast <ankitkp028@gmail.com>
2022-01-06 14:29:04 +0530

    Keeping

---
+++ build/precache-manifest.c0f9c1c57b81c3ff2bca6d4c5d12fe36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de55c147ebbeceafcffc567c1da8decc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a95ab718b547bc61ffa0",
+    "url": "/static/js/2.82995fdd.chunk.js"
+  },
+  {
+    "revision": "b77f7d08fe0174fd1fc2",
+    "url": "/static/js/main.316561f8.chunk.js"
+  },
+  {
+    "revision": "6aba78955beb4a2106d0",
+    "url": "/static/js/runtime-main.761bde41.js"
+  }
+]);
